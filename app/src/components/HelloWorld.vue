16932105feb8e4943a4d0f1eb1198966cbd1f64d
Maciek_dot <maciejd0@op.pl>
2019-07-11 19:18:31 +0200

    commit 1

---
+++ app/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app" ref="app">
+      <slider :width="300" format="push" direction="left" :opacity="0.15"></slider>
+    </div>
+</template>
+<script>
+  import Slider from '@jeremyhamm/vue-slider'
+  export default {
+    name: 'HelloWorld',
+    components: {
+      'slider' : Slider
+    }
+  }
+</script>
+<style lang="less" rel="stylesheet/less" scoped>
+  .icon {
+    &:hover {
+      cursor: pointer;
+    }
+  }
+</style>
+ 
